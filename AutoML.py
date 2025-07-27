373bf0c95f3acf73729d037724eb99174d0c48d1
Pascal van de Wijdeven <pvdwijdeven@gmail.com>
2025-07-27 23:07:30 +0200

    Add command-line arguments for EDA, report file, and output file in AutoML GUI

--- AutoML.py
+++ AutoML.py
@@ -9,6 +9,16 @@ if __name__ == "__main__":
     )
     parser.add_argument("--training_data", help="filename for training data")
     parser.add_argument("--test_data", help="filename for test data")
+    parser.add_argument(
+        "--EDA",
+        help="start EDA, default is False",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--report_file", help="filename for report file (.html)"
+    )
+    parser.add_argument("--output_file", help="filename for output file (.csv)")
     args = parser.parse_args()
     args.project_root = Path(__file__).resolve().parent
     app = wx.App(False)
